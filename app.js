07704063e491e561d11c2d139259c781f853bfea
Augustcode99 <burakodabasi99@yahoo.com>
2023-03-23 04:10:36 +0300

    code for compareHands and chooseWinner funcs

--- app.js
+++ app.js
@@ -1,44 +1,69 @@
 //scores
 let humanScore = document.querySelector(".human span");
 let alienScore = document.querySelector(".alien span");
+const buttons = document.querySelectorAll(".rps-buttons button");
+let alienChoice;
+let humanChoice;
 alienScore = 0;
 humanScore = 0;
-let roundWinner;
-
-//buttons
-const buttons = document.querySelector(".rps-buttons button");
 
 //helper functions
 const rpsAnimation = () => {
   // hand goes up and down 3 times
 };
+
 const rollAlienChoice = () => {
-  const choices = ["rock", "paper", "scissors"];
-  alienChoice = choices[Math.floor(Math.random()) * 3];
-  console.log(alienChoice);
+  const choices = ["Rock", "Paper", "Scissors"];
+  alienChoice = choices[Math.floor(Math.random() * 3)];
+  console.log("alien choice is", alienChoice);
+  console.log("human choice is", humanChoice);
 };
-const chooseWinner = () => {
-  if (roundWinner === "human") {
-    ++humanScore.textContent;
-  } else {
-    ++alienScore.textContent;
+
+const compareHands = () => {
+  if (alienChoice === humanChoice) {
+    console.log("It's a tie");
+    return;
+  }
+  if (humanChoice === "Rock") {
+    if (alienChoice === "Paper") {
+      console.log("Alien wins");
+    } else {
+      console.log("Human wins");
+    }
+    return;
+  }
+  if (humanChoice === "Paper") {
+    if (alienChoice === "Scissors") {
+      console.log("Alien wins");
+    } else {
+      console.log("Human wins");
+    }
+    return;
+  }
+  if (humanChoice === "Scissors") {
+    if (alienChoice === "Rock") {
+      console.log("Alien wins");
+    } else {
+      console.log("Human wins");
+    }
+    return;
   }
 };
 
-const playRound = (e) => {
-  // roundWinner;
+const chooseWinner = () => {
+  rollAlienChoice();
+  compareHands();
+};
+const playRound = () => {
   // hand = rock;
-  // humanChoice = e.name;
-  // rollAlienChoice();
-  // rpsAnimation();
-  // chooseWinner();
-  // hand = humanChoice;
+  rpsAnimation();
+  chooseWinner();
+  //hand = humanChoice
 };
 
 buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    playRound(e);
+  button.addEventListener("click", function () {
+    humanChoice = this.innerHTML;
+    playRound();
   });
 });
-
-rollAlienChoice();
